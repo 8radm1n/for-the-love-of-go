677c8572b9292ab71bfe44c7a50cd7909a58f3ac
Brad Searle <bradleysearle@gmail.com>
2021-03-26 15:47:51 +1000

    warm up chapters

--- 02-data/bookstore.go
+++ 02-data/bookstore.go
@@ -1,8 +1,25 @@
 package bookstore
 
+import (
+	"math/rand"
+)
+
 type Book struct {
 	Title       string
 	Author      string
 	Description string
 	PriceCents  float64
+	ID          int
+}
+
+var Books = []Book{}
+
+func GetAllBooks() []Book {
+	book1 := Book{Title: "Book1"}
+	book2 := Book{Title: "Book2"}
+	return []Book{book1, book2}
+}
+
+func GetNewID() int {
+	return rand.Int()
 }
--- 02-data/bookstore_test.go
+++ 02-data/bookstore_test.go
@@ -3,6 +3,8 @@ package bookstore_test
 import (
 	"bookstore"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestBook(t *testing.T) {
@@ -11,5 +13,27 @@ func TestBook(t *testing.T) {
 		Author:      "jim Bob",
 		Description: "some book ill never read",
 		PriceCents:  1199,
+		ID:          1234567890,
+	}
+}
+
+func TestGetAllBooks(t *testing.T) {
+	book1 := bookstore.Book{Title: "Book1"}
+	book2 := bookstore.Book{Title: "Book2"}
+	bookstore.Books = []bookstore.Book{book1, book2}
+
+	want := bookstore.GetAllBooks()
+	got := bookstore.Books
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetNewID(t *testing.T) {
+	want := 12345678910
+	got := bookstore.GetNewID()
+	if want != got {
+		t.Errorf("want: %d, got: %d", want, got)
 	}
 }
